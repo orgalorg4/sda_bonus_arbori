159b6eb1f2f944d413cb1f7f0d0070d7810a93ba
orgalorg4 <37272144+orgalorg4@users.noreply.github.com>
2018-03-31 12:56:35 +0300

    1

--- sda_bonus_arbori/Source.cpp
+++ sda_bonus_arbori/Source.cpp
@@ -1,6 +1,31 @@
+#include <iostream>
 #include "Header.h"
 
-void main()
+int main()
 {
-	Nod *n = MakeNod(1);
+	Nod *r = 0;
+	Atom n;
+	/*
+	std::cout << "introd sir numere separate prin spatiu terminat cu 0 :";
+	do {
+		std::cin >> n;
+		Insert(r, n);
+	} while (n != 0);
+	while (r)
+	{
+		std::cout << r->data << " ";
+		Delete(r, key(r->data));
+	}
+	std::cout << std::endl;
+	*/
+	r = MakeNod(5);
+	Nod *k = Search(r, 5);
+	//std::cout << key(k->data) << std::endl;
+	if(r)std::cout << key(r->data)<<std::endl;
+	//Delete(r, 5);
+	Nod *p=k;
+	if(r->stg==0)r = r->drt;
+	delete p;
+	if (r)std::cout << key(r->data) << std::endl;
+	return 0;
 }
--- sda_bonus_arbori/functii.cpp
+++ sda_bonus_arbori/functii.cpp
@@ -31,7 +31,7 @@ void Insert(Nod *r, Atom a)
 	else
 	{
 		Nod *p = r;
-		Nod *p1;
+		Nod *p1=p;
 		while (p)
 		{
 			p1 = p;
@@ -48,7 +48,7 @@ void Insert(Nod *r, Atom a)
 		else
 			p1->drt = MakeNod(a);
 	}
-}
+}/*
 void Delete(Nod *&r, int k)
 {
 	if (r == 0)
@@ -60,7 +60,55 @@ void Delete(Nod *&r, int k)
 		if (r2->stg == 0)
 		{
 			p = r2->drt;
-
+			delete r2;
+			r2 = p;
 		}
+		else
+		{
+			if (r2->drt == 0)
+			{
+				p = r2->stg;
+				delete r2;
+				r2 = p;
+			}
+			else
+			{
+				Nod *r3 = r2->stg;
+				while (r3->drt)
+					r3 = r3->drt;
+				p = r3;
+				r3 = r3->stg;
+				p->stg = r3->stg;
+				p->drt = r3->drt;
+				delete r3;
+				r3 = p;
+			}
+		}
+	}
+}
+*/
+
+void Delete(Nod *&r, int k)
+{
+	if (r == 0)
+		return;
+	else
+	{
+		Nod *r2 = Search(r, k);
+		Nod *p=r2;
+		if (r2->stg == 0)r2 = r2->drt;
+		else if (r2->drt == 0)r2 = r2->stg;
+		else
+		{
+			Nod *r3 = r2->stg;
+			while (r3->drt)
+				r3 = r3->drt;
+			p = r3;
+			r3 = r3->stg;
+
+			r3->stg = p->stg;
+			r3->drt = p->drt;
+		}
+		delete p;
 	}
 }
