bf0a2a3df758e5ab6cda5d653edac1e4bf9d5201
orgalorg4 <37272144+orgalorg4@users.noreply.github.com>
2018-04-01 20:11:40 +0300

    1

--- sda_bonus_arbori/Source.cpp
+++ sda_bonus_arbori/Source.cpp
@@ -19,13 +19,17 @@ int main()
 	std::cout << std::endl;
 	*/
 	r = MakeNod(5);
+	
 	Nod *k = Search(r, 5);
+	//Nod *u = NULL;
+	//k = u;
 	//std::cout << key(k->data) << std::endl;
 	if(r)std::cout << key(r->data)<<std::endl;
 	//Delete(r, 5);
 	Nod *p=k;
-	if(r->stg==0)r = r->drt;
+	if(k->stg==0)k = k->drt;
 	delete p;
+	p = 0;
 	if (r)std::cout << key(r->data) << std::endl;
 	return 0;
 }
